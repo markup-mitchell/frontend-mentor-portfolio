00fe365b8bd1ff8cb6dab84fb174d8ba45a9f62e
spitchell <mr.spitchell@gmail.com>
2020-09-02 11:32:17 +0100

    wrap navbar in header in template

--- pages/Template.jsx
+++ pages/Template.jsx
@@ -6,12 +6,12 @@ const Template = ({children}) => {
     <>
     {/* page layout */}
     <div className="w-full px-8 pt-8 md:px-10 md:pt-16 xl:px-0 ">
-      <div className="max-w-6xl mx-auto">
+      <header className="max-w-6xl mx-auto">
         <Navbar />
         <div className="mt-10 md:mt-12 xl:mt-16">
             {children}
         </div>
-      </div>
+      </header>
     </div>
     
     </>
