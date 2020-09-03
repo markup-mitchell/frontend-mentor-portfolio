b4bd3a217b07d4a2946810773a589dfa05e9eba8
spitchell <mr.spitchell@gmail.com>
2020-09-03 21:21:09 +0100

    added poncy transitions

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,9 @@
 import { useRouter } from "next/router";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 
-import '../styles/tailwind.css';
-import Template from './Template';
+import "../styles/tailwind.css";
+import Template from "./Template";
 
 function MyApp({ Component, pageProps }) {
   let [menuOpen, toggleMenu] = useState(false);
@@ -10,20 +11,38 @@ function MyApp({ Component, pageProps }) {
 
   useEffect(() => {
     const handleRouteChange = () => {
-      toggleMenu(false)
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+      toggleMenu(false);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
     // If the component is unmounted, unsubscribe
     // from the event with the `off` method:
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [])
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
   return (
-  <Template menuOpen={menuOpen} toggleMenu={() => toggleMenu(!menuOpen)}>
-  <Component {...pageProps} />
-  </Template> 
-  )
+    <Template menuOpen={menuOpen} toggleMenu={() => toggleMenu(!menuOpen)}>
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        transition={{
+          duration: 0.2,
+          ease: "easeInOut",
+        }}
+        variants={{
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+          },
+        }}
+      >
+        <Component {...pageProps} />
+      </motion.div>
+    </Template>
+  );
 }
 
 export default MyApp;
