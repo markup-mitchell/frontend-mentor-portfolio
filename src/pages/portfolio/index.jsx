7b69036c9288aadfc27cf97028d4a7a6d1b842d9
spitchell <mr.spitchell@gmail.com>
2020-09-05 06:44:56 +0100

    undo deletions

--- src/pages/portfolio/index.jsx
+++ src/pages/portfolio/index.jsx
@@ -17,7 +17,7 @@ const Portfolio = () => {
           </button>
         </PortfolioArticle>
       </section>
-      <section>
+      <section className="pb-16 md:pb-20">
         <PortfolioArticle slug="bookmark" reverse>
           <>Bookmark</>
           <>
@@ -25,6 +25,34 @@ const Portfolio = () => {
             the designs provided. I used HTML5, along with CSS Grid and
             JavaScript for the areas that required interactivity, such as the
             features section.
+          </>
+          <button className="border p-4  whitespace-no-wrap">
+            VIEW PROJECT
+          </button>
+        </PortfolioArticle>
+      </section>
+      <section className="pb-16 md:pb-20">
+        <PortfolioArticle slug="insure">
+          <>Insure</>
+          <>
+            This was a small project which mostly consisted of HTML and CSS. I
+            built a fully-responsive landing page. The only JavaScript this
+            project required was to enable the toggling of the mobile
+            navigation.
+          </>
+          <button className="border p-4  whitespace-no-wrap">
+            VIEW PROJECT
+          </button>
+        </PortfolioArticle>
+      </section>
+      <section>
+        <PortfolioArticle slug="fylo" reverse>
+          <>Fylo</>
+          <>
+            This project was built in pure HTML and CSS. I had mobile and
+            desktop designs to work to and built it so that it was
+            fully-responsive. I took a mobile-first approach and used modern CSS
+            like Flexbox and Grid for layout purposes.
           </>
           <button className="border p-4  whitespace-no-wrap">
             VIEW PROJECT
