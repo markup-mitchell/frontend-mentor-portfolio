9c8f6e94b3277e1aebc7f4286545c6bba687c565
spitchell <mr.spitchell@gmail.com>
2020-09-07 18:54:45 +0100

    add preconnect to fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,28 +1,47 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
-    const initialProps = await NextDocument.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await NextDocument.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
-      <Html lang="en" className="bg-white antialiased">
+      <Html lang="en" className="antialiased bg-white">
         <Head>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/manifest.json" />
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com/css2?family=Ibarra+Real+Nova:wght@400;700&family=Public+Sans:ital,wght@0,100;0,600;0,700;1,700&display=swap"
+            crossorigin
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          {/* <link rel="manifest" href="/manifest.json" /> */}
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#00b4b6" />
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
           <NextScript />
-          <script>{' '}</script>
+          <script> </script>
         </body>
       </Html>
-    )
+    );
   }
 }
